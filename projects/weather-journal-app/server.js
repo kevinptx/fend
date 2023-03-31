694b6b13be908b6bd73ea748867dbd071b937c36
Kevin Peery <kevin.peery@gmail.com>
2023-03-30 19:14:48 -0500

    weather-journal-app complete

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -1,9 +1,14 @@
+const bodyParser = require('body-parser'); //dependency
+const cors = require('cors'); //dependency
+
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +16,29 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
+//GET route that returns the entire projectData object
+app.get('/all', function (req, res) {
+    res.send(projectData);
+});
+
+//POST route that adds incoming data to projectData
+app.post('/add', function (req, res) {
+    //req.body is the data sent from the client side in the POST request. 
+    projectData.temperature = req.body.temperature;
+    projectData.date = req.body.date;
+    projectData.userResponse = req.body.userResponse;
+    res.send(projectData);
+});
 
 // Setup Server
+const port = 8080;
+const server = app.listen(port, listening);
+
+function listening() {
+    console.log(`running on localhost: ${port}`);
+};
