89dc094e004ff1ee0357d506fba89b84ad2896fe
Kevin Peery <kevin.peery@gmail.com>
2023-04-05 18:35:29 -0500

    complete scss files

--- projects/evaluate-news-nlp/src/client/index.js
+++ projects/evaluate-news-nlp/src/client/index.js
@@ -9,6 +9,7 @@ import './styles/base.scss'
 import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
+import './styles/results.scss'
 
 console.log(checkForName);
 console.log(handleSubmit);
