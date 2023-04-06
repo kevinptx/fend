581b72738733780b41eacf4b1948cce1e44522fb
Kevin Peery <kevin.peery@gmail.com>
2023-04-05 19:24:44 -0500

    update testFormHandler.spec.js

--- projects/evaluate-news-nlp/__test__/testFormHandler.spec.js
+++ projects/evaluate-news-nlp/__test__/testFormHandler.spec.js
@@ -1,6 +1,6 @@
 import { handleSubmit } from "../src/client/js/formHandler";
 
-decribe("Testing the functionality for the submission of the form", () => {
+describe("Testing the functionality for the submission of the form", () => {
     test("Testing the handleSubmit() function", () => {
         expect(handleSubmit).toBeDefined();
     })
